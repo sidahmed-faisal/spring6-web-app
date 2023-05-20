8d261594df3bf08a7cfae7e5077bfafc7a565eaa
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2023-05-20 17:27:23 +0200

    added AuthorRepository to implement crudRepository operations

---
+++ src/main/java/sidahmed/springframework/spring6webapp/repositories/AuthorRepository.java
@@ -0,0 +1,7 @@
+package sidahmed.springframework.spring6webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import sidahmed.springframework.spring6webapp.domain.Author;
+
+public interface AuthorRepository extends CrudRepository<Author,Long> {
+}
