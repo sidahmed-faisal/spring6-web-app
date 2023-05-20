155686ff5e8cbe689c6dc92e43366d45fe28034e
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2023-05-20 12:19:06 +0200

    first commit

---
+++ src/test/java/sidahmed/springframework/spring6webapp/Spring6WebAppApplicationTests.java
@@ -0,0 +1,13 @@
+package sidahmed.springframework.spring6webapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Spring6WebAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
