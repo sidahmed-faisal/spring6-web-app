c5a7a89af186ff5b9f7bede9fce62fe45c013b6a
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2023-05-20 17:31:59 +0200

    added BookRepository to implement crudRepository operations

---
+++ src/main/java/sidahmed/springframework/spring6webapp/repositories/BookRepository.java
@@ -0,0 +1,7 @@
+package sidahmed.springframework.spring6webapp.repositories;
+
+import org.springframework.data.repository.CrudRepository;
+import sidahmed.springframework.spring6webapp.domain.Book;
+
+public interface BookRepository extends CrudRepository<Book,Long> {
+}
