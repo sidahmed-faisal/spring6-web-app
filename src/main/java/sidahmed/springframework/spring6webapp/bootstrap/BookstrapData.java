64372c91f3f0a17e2eea819a2f569e92c3a6962e
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2023-05-20 18:26:07 +0200

    add this file to save entities to repos

---
+++ src/main/java/sidahmed/springframework/spring6webapp/bootstrap/BookstrapData.java
@@ -0,0 +1,48 @@
+package sidahmed.springframework.spring6webapp.bootstrap;
+
+import org.springframework.boot.CommandLineRunner;
+import sidahmed.springframework.spring6webapp.domain.Author;
+import sidahmed.springframework.spring6webapp.domain.Book;
+import sidahmed.springframework.spring6webapp.repositories.AuthorRepository;
+import sidahmed.springframework.spring6webapp.repositories.BookRepository;
+
+public class BookstrapData implements CommandLineRunner {
+
+    private final AuthorRepository authorRepository;
+    private final BookRepository bookRepository;
+
+    public BookstrapData(AuthorRepository authorRepository, BookRepository bookRepository) {
+        this.authorRepository = authorRepository;
+        this.bookRepository = bookRepository;
+    }
+
+    @Override
+    public void run(String... args) throws Exception {
+        Author eric = new Author();
+        eric.setFirstName("Eric");
+        eric.setLastName("Evans");
+
+        Book ddd = new Book();
+        ddd.setTitle("Domain Driven Developmet");
+        ddd.setIsbn("123456");
+
+        Author ericSaved = authorRepository.save(eric);
+        Book dddSaved = bookRepository.save(ddd);
+
+
+        Author rod = new Author();
+        eric.setFirstName("Rod");
+        eric.setLastName("Johnson");
+
+        Book noEJB = new Book();
+        ddd.setTitle("J2EE Development without EJB");
+        ddd.setIsbn("325465");
+
+        Author rodSaved = authorRepository.save(rod);
+        Book noEJBSaved = bookRepository.save(noEJB);
+
+        System.out.println("In Bootstrap");
+        System.out.println("Author Count: " + authorRepository.count());
+        System.out.println("Book Count: " + bookRepository.count());
+    }
+}
