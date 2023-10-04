b6d1ba133013946b41e88a11fe72e172bc6cb812
sidahmed-faisal <sidahmedfaisal.sf@gmail.com>
2023-10-04 10:28:13 +0200

    added publisher relation and BookService layer

---
+++ src/main/java/sidahmed/springframework/spring6webapp/services/BookService.java
@@ -0,0 +1,4 @@
+package sidahmed.springframework.spring6webapp.services;
+
+public interface BookService {
+}
---
+++ src/main/java/sidahmed/springframework/spring6webapp/services/BookServiceImpl.java
@@ -0,0 +1,2 @@
+package sidahmed.springframework.spring6webapp.services;public class BookServiceImpl {
+}
